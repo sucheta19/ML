7f6b04e12f18a25af5f1b5d1eb92fd1d72a632a3
sucheta19 <ksucheta.19@gmail.com>
2019-03-27 23:15:48 +0530

    multivariate linera regression

--- excercise1.py
+++ excercise1.py
@@ -202,3 +202,36 @@ pyplot.legend(['Training data', 'Linear regression'])
 pyplot.subplot(1,2,2)
 pyplot.plot(J_history,np.arange(0,iterations,1),'y-')
 pyplot.legend(['Cost function'])
+
+#Visualization
+# grid over which we will calculate J
+theta0_vals = np.linspace(-10, 10, 100)
+theta1_vals = np.linspace(-1, 4, 100)
+
+# initialize J_vals to a matrix of 0's
+J_vals = np.zeros((theta0_vals.shape[0], theta1_vals.shape[0]))
+# Fill out J_vals
+for i, theta0 in enumerate(theta0_vals):
+    for j, theta1 in enumerate(theta1_vals):
+        J_vals[i, j] = computeCost(X, y, [theta0, theta1])
+# Because of the way meshgrids work in the surf command, we need to
+# transpose J_vals before calling surf, or else the axes will be flipped
+J_vals = J_vals.T
+
+# surface plot
+fig = pyplot.figure(figsize=(12, 5))
+ax = fig.add_subplot(121, projection='3d')
+ax.plot_surface(theta0_vals, theta1_vals, J_vals, cmap='viridis')
+pyplot.xlabel('theta0')
+pyplot.ylabel('theta1')
+pyplot.title('Surface')
+
+# contour plot
+# Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+ax = pyplot.subplot(122)
+pyplot.contour(theta0_vals, theta1_vals, J_vals, linewidths=2, cmap='viridis', levels=np.logspace(-2, 3, 20))
+pyplot.xlabel('theta0')
+pyplot.ylabel('theta1')
+pyplot.plot(theta[0], theta[1], 'ro', ms=10, lw=2)
+pyplot.title('Contour, showing minimum')
+pass
